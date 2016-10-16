83ecf75a963e876f15d0656e236bc2fa167a103d
Thomas Skalnik <thomas.skalnik@gmail.com>
2016-10-16 23:22:11 +0200

    fix(test): fixed name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,7 +5,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 
-describe('App: IcmNg', () => {
+describe('App: MysteryAtSchool', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
